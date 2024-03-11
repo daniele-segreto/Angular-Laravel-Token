6c55ecb8fbf6d36b4b6ad8c90b8c5836f1fa1751
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-03-11 11:06:06 +0100

    aggiunta delle rotte di autenticazione (modificato)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +15,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
+    'middleware' => 'api', // Definizione del middleware 'api' per il gruppo di route
+    'prefix' => 'auth' // Definizione del prefisso 'auth' per le route all'interno del gruppo
 ], function ($router) {
-    Route::post('login', 'AuthController@login');
-    Route::post('register', 'AuthController@register');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::get('user-profile', 'AuthController@userProfile');
+    Route::post('/login', [AuthController::class, 'login']); // Route POST per l'endpoint di login gestito dal metodo 'login' dell'AuthController
+    Route::post('/register', [AuthController::class, 'register']); // Route POST per l'endpoint di registrazione gestito dal metodo 'register' dell'AuthController
+    Route::post('/logout', [AuthController::class, 'logout']); // Route POST per l'endpoint di logout gestito dal metodo 'logout' dell'AuthController
+    Route::post('/refresh', [AuthController::class, 'refresh']); // Route POST per l'endpoint di refresh del token gestito dal metodo 'refresh' dell'AuthController
+    Route::get('/user-profile', [AuthController::class, 'userProfile']); // Route GET per l'endpoint del profilo utente gestito dal metodo 'userProfile' dell'AuthController
 });
+
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'auth'
+
+// ], function ($router) {
+//     Route::post('login', 'AuthController@login');
+//     Route::post('register', 'AuthController@register');
+//     Route::post('logout', 'AuthController@logout');
+//     Route::post('refresh', 'AuthController@refresh');
+//     Route::get('user-profile', 'AuthController@userProfile');
+// });
